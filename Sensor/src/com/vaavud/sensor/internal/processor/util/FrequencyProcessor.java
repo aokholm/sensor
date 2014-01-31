154e3998094bdc8f63845a21f36929825eead72b
Andreas Bruun Okholm <aokholm@gmail.com>
2014-01-31 12:54:15 +0100

    Improved frequency processor response to breaks in the measurements.

--- Sensor/src/com/vaavud/sensor/internal/processor/util/FrequencyProcessor.java
+++ Sensor/src/com/vaavud/sensor/internal/processor/util/FrequencyProcessor.java
@@ -21,10 +21,19 @@ public class FrequencyProcessor {
     return getFrequency(start, end, N);
   }
   
-  public Double getFrequencyPoints(int nPoints) {
+  public Double getFrequencyPoints(int nPoints, long Us) {
+    if (nPoints < 50) {
+      return lastFrequency;
+    }
+    
     List<SensorEvent> events= list.getLastEvents(nPoints);
     Long start = events.get(0).timeUs;
     Long end = events.get(events.size() -1).timeUs;
+    if (end-start > Us) {
+      System.out.println("Divide N: " + nPoints + " start: " + start + "end: " + end);
+      return getFrequencyPoints(nPoints/2, Us);
+    }
+    
     Integer N = events.size();
     
     return getFrequency(start,  end, N);
@@ -32,8 +41,7 @@ public class FrequencyProcessor {
   
   public Double getFrequency(Long Us) {
     int nPoints = (int) (Us*lastFrequency / 1000000);
-    
-    return getFrequencyPoints(nPoints);
+    return getFrequencyPoints(nPoints, (long) (Us*1.1f));
   }
   
   private Double getFrequency(long start, long end, long N) {
